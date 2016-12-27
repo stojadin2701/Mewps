155fda89e0dfcec91239f50280c7548af7a391d8
schutzekatze <vladimir.nik94@gmail.com>
2016-12-27 23:36:59 +0100

    Add microcontroller makefile

---
+++ Microcontroller/bin/WInterrupts.d
@@ -0,0 +1,14 @@
+bin/WInterrupts.c.o bin/WInterrupts.S bin/WInterrupts.d: \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/WInterrupts.c \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/wiring_private.h \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/variants/standard/pins_arduino.h
+
+/usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/wiring_private.h:
+
+/usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/Arduino.h:
+
+/usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/binary.h:
+
+/usr/share/arduino/hardware/archlinux-arduino/avr/variants/standard/pins_arduino.h:
---
+++ Microcontroller/bin/new.d
@@ -0,0 +1,2 @@
+bin/new.cpp.o bin/new.S bin/new.d: \
+ /usr/share/arduino/hardware/archlinux-arduino/avr/cores/arduino/new.cpp
