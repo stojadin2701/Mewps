71d1bf3b352976b08e9f7e9045bcba68bcb2ddbc
stojadin2701 <stojadin2701@gmail.com>
2017-02-25 15:48:02 +0000

    Frontal obstacle avoidance

--- src/main.cpp
+++ src/main.cpp
@@ -29,16 +29,18 @@ void distanceThread(){
 	for(int i=0; i<300; i++){
 		distance = DistanceSensor::get_distance();
 		cout << "Distance: " << distance << endl;
-		if(distance<=10){
+		if(distance<=20){
 			stop=true;
-			Motors::set_powers(-0.7,-0.7);
-			sleep_for(milliseconds(1500));
+			Motors::set_powers(-0.5,-0.5);
+			sleep_for(milliseconds(1000));
 			Motors::set_powers(0,0);
 			break;
 		}
 	
-		sleep_for(milliseconds(600));
+		sleep_for(milliseconds(35));
 	}
+
+
 }
 
 int main()
@@ -52,6 +54,7 @@ int main()
 		try
 		{
 			Motors::set_powers(0.5,0.5);
+			sleep_for(milliseond(3000));
 			for(int i=0; i<30; i++){
 					float f1, f2, f3;
 					Microphones::get_intensities(&f1, &f2, &f3);
@@ -59,6 +62,22 @@ int main()
 					sleep_for(milliseconds(300));
 				}
 			Motors::set_powers(0,0);
+			if(stop){
+				sleep_for(milliseconds(300));
+				Motors::set_powers(0.75, -0.8);
+				sleep_for(milliseconds(1000));
+				Motors::set_powers(0, 0);
+				sleep_for(milliseconds(500));	
+				Motors::set_powers(0.5, 0.5);
+				sleep_for(milliseconds(1000));
+				Motors::set_powers(0,0);
+				sleep_for(milliseconds(500));
+				Motors::set_powers(-0.8, 0.65);
+				sleep_for(milliseconds(1000));
+				Motors::set_powers(0,0);
+			}
+
+			
 		}
 		catch (exception &e)
 		{
