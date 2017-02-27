3ffef42899781b35508068dafb86daefbf26c68c
stojadin2701 <stojadin2701@gmail.com>
2017-02-27 21:06:40 +0100

    Recovery thread test

--- src/main.cpp
+++ src/main.cpp
@@ -22,26 +22,39 @@ using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
 
 #include <mutex>
+#include <condition_variable>
 using std::mutex;
 using std::unique_lock;
+using std::condition_variable;
 
-mutex recover;
+mutex stop, complete;
+condition_variable cv;
 
-bool stop = false;
+bool kill = false;//kill recoveryThread
+bool ready = true; //ready to listen
+
+enum Direction { FRONT, RIGHT, LEFT, FRONT_SHORT, NONE };
+
+//std::atomic<Direction> sound_direction {NONE};
+Direction sound_direction = NONE;
 
 void distanceThread(){
 	int16_t distance;
 	for(int i=0; i<200; i++){
 		distance = DistanceSensor::get_distance();
 		cout << "Distance: " << distance << endl;
+
 		if(distance<=20){
-			unique_lock<mutex> lock(recover);
-			stop = true;
-			lock.unlock();
-			Motors::set_powers(-0.5,-0.5);
-			sleep_for(milliseconds(1000));
 			Motors::set_powers(0,0);
-			//	break;
+			unique_lock<mutex> lock_stop(stop);
+			kill = true; //kill recoveryThread if exists
+			lock_stop.unlock();
+
+			unique_lock<mutex> lock_complete(complete);
+			ready = false;
+			lock_complete.unlock();
+
+			thread recThread(recoveryThread);
 		}
 
 		sleep_for(milliseconds(35));
@@ -49,7 +62,11 @@ void distanceThread(){
 }
 
 void recoveryThread(){
+	bool check;
 	cout<<"RECOVERING"<<endl;
+	Motors::set_powers(-0.5,-0.5);
+	sleep_for(milliseconds(1000));
+	Motors::set_powers(0,0);
 	sleep_for(milliseconds(300));
 	Motors::set_powers(0.75, -0.8);
 	sleep_for(milliseconds(1000));
@@ -57,15 +74,26 @@ void recoveryThread(){
 	sleep_for(milliseconds(500));
 	Motors::set_powers(0.5, 0.5);
 	sleep_for(milliseconds(1000));
+	//kill
+	unique_lock<mutex> lock_stop(stop);
+	check = kill;
+	kill = false;
+	lock_stop.unlock();
+	if(check)std::terminate();
+
 	Motors::set_powers(0,0);
 	sleep_for(milliseconds(500));
 	Motors::set_powers(-0.8, 0.65);
 	sleep_for(milliseconds(1000));
 	Motors::set_powers(0,0);
+	cout<<"RECOVERY COMPLETE"<<endl;
+	//set atomic bool
+	unique_lock<mutex> lock_complete(complete);
+	ready = true;
+	lock_complete.unlock();
+	cv.notify_one();
 }
 
-//enum Direction { FRONT, RIGHT, LEFT, FRONT_RIGHT, FRONT_LEFT,  NONE };
-enum Direction { FRONT, RIGHT, LEFT, FRONT_SHORT, NONE };
 
 Direction calculate_direction( int16_t i1, int16_t i2, int16_t i3){
 	Direction dir = NONE;
@@ -76,7 +104,7 @@ Direction calculate_direction( int16_t i1, int16_t i2, int16_t i3){
 	else if((i1-eps)>= i2 && (i1-eps)>=i3) dir = FRONT;
 	else if((i2-eps)>= i1 && (i2-eps)>=i3) dir = RIGHT;
 	else if((i3-eps)>= i1 && (i3-eps)>=i2) dir = LEFT;
-	else if((i2+eps)<= i1 && (i2+eps)<=i3) dir = FRONT_SHORT; 	
+	else if((i2+eps)<= i1 && (i2+eps)<=i3) dir = FRONT_SHORT;
 	else if((i3+eps)<= i1 && (i3+eps)<=i2) dir = FRONT_SHORT;
 
 	return dir;
@@ -96,10 +124,17 @@ int main()
 			//for(int i=0; i<30; i++){
 			int16_t f1, f2, f3;
 			sleep_for(milliseconds(2000));
+
+			unique_lock<mutex> lock_complete(complete);
+			cv.wait(lock_complete, []{return ready;});
+			lock_complete.unlock();
+			cout<<"LISTENING"<<endl;
+
+
 			Microphones::get_intensities(&f1, &f2, &f3);
 			cout << "Front: " << f1 << " Right: " << f2 << " Left: "<< f3 << endl;
-			Direction dir = calculate_direction(f1, f2, f3);
-			switch(dir){
+			sound_direction = calculate_direction(f1, f2, f3);
+			switch(sound_direction){
 				case FRONT:
 					cout<<"FRONT"<<endl;
 					Motors::set_powers(0.5,0.5);
@@ -131,30 +166,6 @@ int main()
 					break;
 			}
 
-			unique_lock<mutex> lock(recover);
-			if(stop){
-				stop = false;
-				lock.unlock();
-				cout<<"RECOVERING"<<endl;
-				sleep_for(milliseconds(300));
-				Motors::set_powers(0.75, -0.8);
-				sleep_for(milliseconds(1000));
-				Motors::set_powers(0, 0);
-				sleep_for(milliseconds(500));
-				Motors::set_powers(0.5, 0.5);
-				sleep_for(milliseconds(1000));
-				Motors::set_powers(0,0);
-				sleep_for(milliseconds(500));
-				Motors::set_powers(-0.8, 0.65);
-				sleep_for(milliseconds(1000));
-				Motors::set_powers(0,0);
-				lock.lock();
-				stop = false;
-				lock.unlock();
-
-			}else{
-				lock.unlock();
-			}
 
 		}
 
